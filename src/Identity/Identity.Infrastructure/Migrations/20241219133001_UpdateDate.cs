cb6e2fd63afac74964b2ce76febcacbf4e179938
sayyedulawwab <38081373+sayyedulawwab@users.noreply.github.com>
2024-12-19 21:52:51 +0600

    update

---
+++ src/Identity/Identity.Infrastructure/Migrations/20241219133001_UpdateDate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Identity.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UpdatedOn",
+                table: "Users",
+                newName: "UpdatedOnUtc");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedOn",
+                table: "Users",
+                newName: "CreatedOnUtc");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UpdatedOnUtc",
+                table: "Users",
+                newName: "UpdatedOn");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedOnUtc",
+                table: "Users",
+                newName: "CreatedOn");
+        }
+    }
+}
--- src/Ordering/Ordering.Infrastructure/Migrations/20241219133031_UpdateDate.Designer.cs
+++ src/Ordering/Ordering.Infrastructure/Migrations/20241219133031_UpdateDate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Ordering.Infrastructure;
 
@@ -11,9 +12,11 @@ using Ordering.Infrastructure;
 namespace Ordering.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241219133031_UpdateDate")]
+    partial class UpdateDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
